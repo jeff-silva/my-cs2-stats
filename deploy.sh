177d43bd65e6ede89b9608cd9df3b11f7ca4763a
Jeferson Silva <jeferson.i.silva@gmail.com>
2025-06-10 14:08:11 -0300

    feat: stats update

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ cd /home/jeff/my-cs2-stats
 /home/jeff/.nvm/versions/node/v22.14.0/bin/yarn dev
 /usr/bin/git add .
 /usr/bin/git commit -m "feat: stats update"
-/usr/bin/git push
+"/usr/bin/git push https://jeff-silva:${GIT_TOKEN}@github.com/jeff-silva/my-cs2-stats.git"
